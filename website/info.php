7181772b25a1543dc3d062a3ac94ac43a1d3c63c
NITHYA-RR <nithyaramesh192004@gmail.com>
2024-12-21 19:08:50 +0530

    second

--- website/info.php
+++ website/info.php
@@ -1,3 +1,10 @@
+
+
 <?php
-echo("haii")
+// Check if mysqli extension is loaded
+if (extension_loaded('mysqli')) {
+    echo "mysqli is enabled!";
+} else {
+    echo "mysqli is not enabled!";
+}
 ?>
--- website/libs/load.php
+++ website/libs/load.php
@@ -1,31 +1,75 @@
 <?php
-function load_templates($name){
-    include __DIR__."/../__templates/$name.php";
+function load_templates($name) {
+    include __DIR__ . "/../__templates/$name.php";
 }
 
-function validate_function($username,$password){
- 
-    if($username=="nithya@gmail.com" and $password=="password"){
+function validate_function($username, $password) {
+    if ($username == "nithya@gmail.com" and $password == "password") {
         return true;
-    }
-    else{
+    } else {
         return false;
     }
 }
-function signup($username, $password, $email, $phone){
-   $servername = "phpMyAdimn";
-   $username = "root";
-   $password = "3306";
-   $dbname = "collect the data";
 
-// Create connection
+function signup($email, $phone) {
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "php";
+
+    // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-      if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-}else{
-  print("success"); 
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } else {
+        echo "Success";
+    }
+
+    // Add logic to insert the data into a database table
+    // Example:
+    // $sql = "INSERT INTO users (email, phone) VALUES ('$email', '$phone')";
+    // if ($conn->query($sql) === TRUE) {
+    //     echo "New record created successfully";
+    // } else {
+    //     echo "Error: " . $sql . "<br>" . $conn->error;
+    // }
+
+    $conn->close();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // // $sql = "INSERT INTO MyGuests (firstname, lastname, email)
@@ -40,4 +84,4 @@ function signup($username, $password, $email, $phone){
 
 // $conn->close();
 // }
-}
+// }
